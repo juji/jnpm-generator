569a9c1654f48ac2c8110a1c7b043e7e9e8923ad
juji <juji@komputerjuj.(none)>
2014-08-17 01:33:13 +0700

    auto-commit

--- modules/push.js
+++ modules/push.js
@@ -2,7 +2,7 @@ var exec = require('shelljs').exec;
 
 require('./commit.js');
 
-if(exec('git push').code) {
+if(exec('git push origin master').code) {
 	console.log('ERROR: Git push failed');
 	process.exit();
 }
